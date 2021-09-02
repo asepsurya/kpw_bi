990e60dfd69fb77094ffdfa879a98860b28f49f1
Asep <asepsurya1998@gmail.com>
2021-09-02 20:26:03 +0700

    update Dd

--- kpw_bi/DB/kpw_bi.sql
+++ kpw_bi/DB/kpw_bi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 02 Sep 2021 pada 12.46
+-- Waktu pembuatan: 02 Sep 2021 pada 15.25
 -- Versi server: 10.4.19-MariaDB
 -- Versi PHP: 8.0.6
 
@@ -137,7 +137,7 @@ INSERT INTO `tb_formulir` (`id_ikm`, `tanggal`, `jenis_produk`, `merek`, `kompos
 (1, '02-09-2021', 'Refile Parfume', 'D\'Laza', 'asdasd', 'asdasd', 'Wangi dan hanagat', '-', 200, 0, '50000', '', '', 'Anak Muda', '', 'Cv. Muliana', '234324', '23432432', 'NIB = 0255448845', '     Intagaram, Whatsapp, Facebook, Tokopedia', 'Lorem Ipsum dolor', 'asdasd'),
 (2, '02-09-2021', 'asdad', 'asdasd', 'adasd', 'asdasd', 'adasd', 'ne', 100, 0, '500', 'hari', 'Bulan', 'adasd', '', 'Cv. Muliana', '', '', '', 'adasdas', 'Lorem Ipsum dolor', 'adsadas'),
 (8, '02-09-2021', 'Bakso', 'Nilon', 'adas', 'asdas', 'Wangi dan hanagat', '-', 500, 0, '5000', '', '', 'Anak Muda', '', 'Cv. Muliana', '', '', ' ', ' adas', 'Lorem Ipsum dolor', 'ada'),
-(9, '02-09-2021', 'fsfsd', 'sfdds', 'sdfsdf', 'sdfdssfd', 'fdsfds', '-', 100, 0, '45000', 'hari', 'Bulan', 'fsdfds', '', 'sdfdf', '', '', '', '-', 'Lorem Ipsum dolor', 'sdfd');
+(9, '02-09-2021', 'teh hijau', 'TACHA', 'teh hijau murni', 'serbuk dan daun', 'ALAMI', '-', 100, 0, '45000', '', '', 'Mnengah ke atas', '', 'CV INOPAK', '1231425', '12345', '  -', '  INSTAGRAM', 'Lorem Ipsum dolor', 'nbcvfxvczfshmgcv');
 
 -- --------------------------------------------------------
 
@@ -149,6 +149,46 @@ CREATE TABLE `tb_kota` (
   `no` int(11) NOT NULL,
   `id_kota` varchar(30) NOT NULL,
   `nama_kota` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_kurasi`
+--
+
+CREATE TABLE `tb_kurasi` (
+  `id_ikm` varchar(30) NOT NULL,
+  `supply` text NOT NULL,
+  `proses_pengolahan` text NOT NULL,
+  `kapasitas_produksi` text NOT NULL,
+  `konsistensi_produksi` text NOT NULL,
+  `tempat_produksi` text NOT NULL,
+  `keterlibatan` text NOT NULL,
+  `dampak` text NOT NULL,
+  `kearifan` text NOT NULL,
+  `kreativitas` text NOT NULL,
+  `citra_rasa` text NOT NULL,
+  `varian` text NOT NULL,
+  `keunikan` text NOT NULL,
+  `perijinan_dasar` text NOT NULL,
+  `perijinan_tingkat` text NOT NULL,
+  `jenis_kemasan` text NOT NULL,
+  `visual` text NOT NULL,
+  `attribut` text NOT NULL,
+  `daya_tahan` text NOT NULL,
+  `struktur` text NOT NULL,
+  `administrasi` text NOT NULL,
+  `keuangan` text NOT NULL,
+  `retail` text NOT NULL,
+  `b_t_b` text NOT NULL,
+  `export` text NOT NULL,
+  `b_t_c` text NOT NULL,
+  `reseller` text NOT NULL,
+  `media` text NOT NULL,
+  `market` text NOT NULL,
+  `website` text NOT NULL,
+  `e-payment` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -178,7 +218,7 @@ INSERT INTO `tb_legalitas` (`id_ikm`, `nib`, `spirt`, `layak_sehat`, `halal`, `c
 (1, '10052244788', '23432432', '13132', '234324', '23423', '9555122111', '1205522114555', '4654', 'NIB = 0255448845'),
 (2, '10052244788', '', '', '', '', '9555122111', '', '', ''),
 (8, '100522447889', '', '', '', '', '', '', '', ' '),
-(9, '', '', '', '', '', '', '', '', '');
+(9, '123', '12345', '13245235', '1231425', '12323535', '1100', 'REGISTER', '1325235', '  -');
 
 -- --------------------------------------------------------
 
@@ -271,7 +311,7 @@ INSERT INTO `tb_ukm` (`id_ikm`, `id_kota`, `nama`, `alamat`, `gender`, `kelas`, 
 (1, '550', 'Asep Surya Somantri', 'Ruko Hamas, Jl. Pancasila No.1, Lengkongsari, Tawang, Tasikmalaya, West Java', 'L', ' asepsurya1998@gmail.com', '  (+62)81-323-899-376'),
 (2, '550', 'Eka Wijaya', 'asdad', 'L', 'iin201@yahoo.com', '(+62)81-323-899-376'),
 (8, '550', 'Rani Nurnawati', 'Tasikmalaya', 'P', ' asdasd@gmail.com', ' (+62)81-323-899-376'),
-(9, '550', 'Rani', 'dsdf', 'P', 'rani@gmail.com', '081214713475');
+(9, '550', 'Rani', 'Ciamis', 'P', '  rani@gmail.com', '  081214713475');
 
 -- --------------------------------------------------------
 
@@ -299,7 +339,10 @@ INSERT INTO `user` (`id_ikm`, `nama`, `username`, `password`, `level`, `id_kota`
 (2, 'Asep Surya  Somantri', 'asepsurya1998@gmail.com', '21232f297a57a5a743894a0e4a801fc3', 1, 'KT001', 'a.jpg', 1),
 (6, 'IIN SETIWAN', 'iinsetiawan2021@gmail.com', '21232f297a57a5a743894a0e4a801fc3', 2, 'KT001', 'a.jpg', 1),
 (8, 'Rani Nurnawati', '081323899376', '0938fe42b5ee7cddf56bceddd4ece4ff', 2, '', 'a.jpg', 0),
-(9, 'Rani', '081214713475', 'bfd925fa86084bd0300fde7fd05ddd97', 2, '', 'a.jpg', 0);
+(9, 'Rani', '081214713475', 'bfd925fa86084bd0300fde7fd05ddd97', 2, '', 'a.jpg', 0),
+(10, 'Kurator 1', 'kurator1@email.com', '21232f297a57a5a743894a0e4a801fc3', 12, '65554', '', 0),
+(12, 'Kurator 2', 'kurator2@email.com', '21232f297a57a5a743894a0e4a801fc3', 13, '65554', '', 0),
+(13, 'Kurator 3', 'kurator3@email.com', '21232f297a57a5a743894a0e4a801fc3', 14, '65554', '', 0);
 
 -- --------------------------------------------------------
 
@@ -337,6 +380,12 @@ ALTER TABLE `tb_formulir`
 --
 ALTER TABLE `tb_kota`
   ADD PRIMARY KEY (`no`);
+
+--
+-- Indeks untuk tabel `tb_kurasi`
+--
+ALTER TABLE `tb_kurasi`
+  ADD PRIMARY KEY (`id_ikm`);
 
 --
 -- Indeks untuk tabel `tb_legalitas`
@@ -406,7 +455,7 @@ ALTER TABLE `tb_pengajuan`
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_ikm` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_ikm` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
